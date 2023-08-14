8a5b56aa24298f46fee028b08785a60cfb7ed8c5
HylintDLC <141586153+HylintDLC@users.noreply.github.com>
2023-08-14 17:34:26 -0400

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,32 @@
+-----------------Pour obtenir de l'aide, des scripts, etc.----------------
+--------------- https://discord.gg/c9ZMqvMUEE  ---------------------------
+--------------------------------------------------------------------------
+
+
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+description "Hylint Ascenseur ESX/QbCore"
+
+author 'hylint'
+
+version '1.0.2'
+
+client_scripts {
+	'client/*.lua',
+}
+
+server_scripts {
+	'server/*.lua'
+}
+
+shared_scripts {
+	'@ox_lib/init.lua',
+	'config.lua'
+}
+
+dependencies {
+	'ox_lib',
+	'ox_target'
+}
